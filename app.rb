decd119b1364ed61035168a9bf09a46286465048
jennynguyen <jennynguyen622@gmail.com>
2014-09-08 17:34:55 -0400

    add: show 10 posts at a time

--- app.rb
+++ app.rb
@@ -21,8 +21,6 @@ class App < Sinatra::Base
   end
 
   after do
-    #Happy note: I can use logger.info "_: #{}" anywhere
-    #to check my errors.
     logger.info "Response Headers: #{response.headers}"
   end
 
@@ -30,39 +28,42 @@ class App < Sinatra::Base
   # DB Configuration
   ########################
 
-  #ENV["REDISTOGO_URL"]) is saved in my .bash_profile
+  #ENV["REDISTOGO_URL"]) is saved in .bash_profile
+  #reset redis by adding .fushdb
   $redis = Redis.new(:url => ENV["REDISTOGO_URL"])
+  # $redis.flushdb
 
   ########################
   # Routes
   ########################
 
+  #main page
   get('/') do
-    #my main page
-    # render(:erb, :home_page)
-    # redirect to("/posts")
     redirect to("/signup")
   end
-
 
   get("/signup") do
     render(:erb, :signup)
   end
 
-
   get("/signin") do
     render(:erb, :signin)
   end
 
-
   # GET /posts
   get("/posts") do
-     @posts = $redis.keys("*posts*").map { |post| JSON.parse($redis.get(post)) }
+    id = params["first"].to_i || 0
+    #iterate through all the datas' keys stored as redis
+    #return a new array with json database format
+    #so that I can call methods on them
+     posts = $redis.keys("*posts*").map { |post| JSON.parse($redis.get(post)) }
+     # @posts = $redis.keys("*posts*").map { |post| JSON.parse($redis.get(post)) }
+    @posts = posts[id, 10]
     render(:erb, :index)
   end
 
-
-  # POST /posts
+  # create a new post, increase counter by 1
+  #
   post("/posts") do
     # passing parameters from post
     title = params[:title]
@@ -73,11 +74,8 @@ class App < Sinatra::Base
     #keys are: index, title, content
     post = { id: index, title: title, content: content}
     # Set json form with key in redis
-    #Happy note: need to have a space between "post: #{}"
-    #or else the code won't work.
-    #learned to use logger.info "_: #{}" here to stop and check for error
-    #is it similar to binding.pry??????
     $redis.set("posts: #{index}", post.to_json)
+    # redirect go /posts
     redirect to("/posts")
   end
 
@@ -88,9 +86,6 @@ class App < Sinatra::Base
 
   # GET /posts/1
   get("/posts/:id") do
-    #_id, _raw_post are local variables.
-    #They only exist here within this method.
-    #Happy note: spent 18 hours to figure it out. Totally worth it!
     _id = params[:id]
     _raw_post = $redis.get("posts: #{_id}")
     @post = JSON.parse(_raw_post)
@@ -106,12 +101,13 @@ class App < Sinatra::Base
   end
 
   # PUT /posts/1
-  #update post base
+  #update post using id
   put("/posts/:id") do
     _title = params[:new_post_title]
     _content = params[:new_post_content]
     _id = params[:id]
     updated_post = {id: _id, title: _title, content: _content}
+    #$redis key is "post: #{_id}"
     $redis.set("posts: #{_id}", updated_post.to_json)
     redirect to("/posts")
   end
@@ -127,19 +123,18 @@ class App < Sinatra::Base
   #NEED TO FIRGURE OUT HOW TO ALLOW USERS TO REALLY BE ABLE TO SIGN UP!!!
   #as for now sign up ang sign in don't work => they both lead to the posts I wrote.
   post ('/signup') do
-    redirect("/posts")
+      redirect("/posts")
   end
 
 
   #for users to sign up
   #same problem as above. Merde!!!!!!!
   post ('/signin') do
+    # codes don't work. merde!
+    # before '/posts/*' do
+    #  authenticate!
+    # end
     redirect("/posts")
   end
-
-
-
-
-
 
 end
